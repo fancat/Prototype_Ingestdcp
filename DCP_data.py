33a39379ac2f3c7adef04d61b2694f096520d7f3
Maeva C <maeva.christian@etu.enseeiht.fr>
2017-07-19 01:38:17 +0200

    Add Movie class

--- DCP_data.py
+++ DCP_data.py
@@ -3,13 +3,15 @@ from __init__ import db
 
 class DCP_data(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
-    source_directory=db.Column(db.String(200))
-    target_directory=db.Column(db.String(200))
-    movie=db.Column(db.String(80))
-    copy_result=db.Column(db.String(80))
-    copy_err=db.Column(db.String(200))
-    check_result=db.Column(db.String(20))
-    check_err=db.Column(db.String(200))
-    check_hash_result=db.Column(db.String(20))
-    check_hash_err=db.Column(db.String(200))
+    source_directory = db.Column(db.String(200))
+    target_directory = db.Column(db.String(200))
+    copy_result = db.Column(db.String(80))
+    copy_err = db.Column(db.String(200))
+    check_result = db.Column(db.String(20))
+    check_err = db.Column(db.String(200))
+    check_hash_result = db.Column(db.String(20))
+    check_hash_err = db.Column(db.String(200))
+
+    movie_id = db.Column(db.Integer, db.ForeignKey("movie_data.id"))
+    movie = db.relationship("Movie_data", back_populates="dcps")
 
---
+++ Movie_data.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+from __init__ import db
+
+class Movie_data(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(80))
+    original_title = db.Column(db.String(80))
+    release_date = db.Column(db.DateTime)
+    description = db.Column(db.Text)
+    category = db.Column(db.String(10))
+
+    dcps = db.relationship("DCP_data", back_populates="movie")
+
